2614733ae135830c996e0101f16f292d41974adb
yashprakash13 <yashprakash13@gmail.com>
2019-09-05 10:01:04 +0530

    menu button js and rotate

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+//select DOM items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItmes = document.querySelectorAll(".nav-item");
+
+//set intial menu state
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+
+    navItmes.forEach(item => item.classList.add("show"));
+    //set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+
+    navItmes.forEach(item => item.classList.remove("show"));
+    //set menu state
+    showMenu = false;
+  }
+}
